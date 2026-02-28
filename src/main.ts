7dc553d46575b9056403a547b9bea96f70361435
Peter Wang <peterwangsc@gmail.com>
2026-02-28 02:47:29 -0800

    Add analytics back

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import "./style.css";
 import { initSupabase } from "./util/Supabase";
 import { CONFIG } from "./game/Config";
-// import { inject } from "@vercel/analytics";
+import { inject } from "@vercel/analytics";
 
-// inject();
+inject();
 
 const app = document.getElementById("app");
 if (!app) throw new Error("Missing #app root element");
